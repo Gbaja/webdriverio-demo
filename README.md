75800e8b4fda37313efac0efe6f11cb5c47575b7
Fatimat Gbajabiamila <fatimat@Fatimats-MacBook-Pro.local>
2020-01-09 14:30:38 +0000

    Added more homepage test

---
+++ README.md
@@ -0,0 +1,3 @@
+To run all tests: ./node_modules/.bin/wdio wdio.conf.js
+
+To run specific file: ./node_modules/.bin/wdio wdio.conf.js --spec <//FILENAME//>
--- test/specs/homepage.js
+++ test/specs/homepage.js
@@ -1,11 +1,46 @@
 import assert from 'assert';
 
 describe('Apolitical home page', () => {
- it('should have the right title', () => {
-  browser.url("https://apolitical.co/home");
-  const title = browser.getTitle();
-  console.log("title: ", title);
-  
-  assert.strictEqual(title, "Home | Apolitical");
- });
+	it('should have the right title', () => {
+		browser.url("http://localhost:8081/home");
+		const title = browser.getTitle();
+
+		assert.strictEqual(title, "Home | Apolitical");
+	});
+
+	it('should render the welcome text when logged out', () => {
+		browser.url("http://localhost:8081/home");
+		const LoggedOutHomePageHero = browser.react$('LoggedOutHomePageHero');
+
+		assert.equal(LoggedOutHomePageHero.isDisplayed(), true);
+		assert.equal(LoggedOutHomePageHero.getText(), 'Welcome to Apolitical\nThe global learning platform for government');
+	});
+
+	it('has a login anchor link', () => {
+		browser.url("http://localhost:8081/home");
+		const loginButton = $('a[href="/login"');
+
+		assert.equal(loginButton.isExisting(), true);
+	}); 
+
+	it('clicking "Get Free Access now" button links to sign up page', () => {
+		browser.url("http://localhost:8081/home"); 
+		const getAcessButton =  $('button[data-gtm-event-type="signup-click"]');
+
+		getAcessButton.click();
+
+		const pageUrl =  browser.getUrl();
+
+		assert.equal(pageUrl, 'http://localhost:8081/signup?ref=feed-unlock-banner');
+	})
+
+	it('links to log in page', () => {
+		browser.url("http://localhost:8081/home");
+		const loginButton = $('=Log In');
+
+		loginButton.click();
+
+		const pageUrl =  browser.getUrl();
+		assert.equal(pageUrl, 'http://localhost:8081/login');
+	})
 })
